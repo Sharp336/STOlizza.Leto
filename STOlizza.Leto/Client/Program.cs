46500209a8a1b2d11a57799a01e290d4c11ddfd2
Sharp <Sharp@SHARPPC>
2023-05-28 23:33:14 +0300

    Smena service

--- STOlizza.Leto/Client/Program.cs
+++ STOlizza.Leto/Client/Program.cs
@@ -13,6 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IQuestionnaireService, QuestionnaireService>();
+builder.Services.AddScoped<ISmenaService, SmenaService>();
 
 builder.Services.AddMudServices();
 
---
+++ STOlizza.Leto/Shared/SmenaDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace STOlizza.Leto.Shared
+{
+    public class SmenaDTO
+    {
+        public int number;
+        public string title;
+        public string description;
+        public string dates;
+        public string color;
+        public bool isAvailable;
+
+        public SmenaDTO(int _number, string _title, string _description, string _dates, string _colour, bool _isavailable)
+        {
+            number = _number;
+            title = _title;
+            description = _description;
+            dates = _dates;
+            color = _colour;
+            isAvailable = _isavailable;
+        }
+    }
+}
