a19f84aa2f07c01f17703faaf2dee8844549ae21
Sharp <Sharp@SHARPPC>
2023-06-01 15:16:26 +0300

    правки

--- STOlizza.Leto/Server/Program.cs
+++ STOlizza.Leto/Server/Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<DatabaseContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("LocalConnectionString"))); 
+builder.Services.AddDbContext<DatabaseContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("LocalConnectionString")));
+//builder.Services.AddDbContext<DatabaseContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("RemoteConnectionString")));
 
 builder.Services.AddCoreAdmin();
 
