98a4d7b0371655c3480bd34f1cf7e53cf2c8ff79
Sharp <Sharp@SHARPPC>
2023-06-04 18:23:57 +0300

    правки

--- STOlizza.Leto/Server/DataSeeder.cs
+++ STOlizza.Leto/Server/DataSeeder.cs
@@ -20,7 +20,7 @@ namespace STOlizza.Leto.Server
 
             var DefaultSmenaList = new List<SmenaDTO>(){
                 new SmenaDTO(){number = 1, slogan="ПОМНИ", title = "Патриотика", description = "Для тех, кто любит свою Родину.", dates = "20-23 июня", color = "#701487", availableTill = "10 июня", isAvailable = true},
-                new SmenaDTO(){number = 2, slogan="УЗНАВАЙ", title = "Медиа", description = "Научит создавать инфоповоды и развиваться в медиа поле.", dates = "26-29 июня", color = "#701487", availableTill = "16 июня", isAvailable = true},
+                new SmenaDTO(){number = 2, slogan="УЗНАВАЙ", title = "Медиа", description = "Научит создавать инфоповоды и развиваться в медиаполе.", dates = "26-29 июня", color = "#701487", availableTill = "16 июня", isAvailable = true},
                 new SmenaDTO(){number = 3, slogan="ДОСТИГАЙ", title = "Спорт", description = "Научит побеждать и развивать свою физическую силу.", dates = "1-4 июля", color = "#701487", availableTill = "21 июня", isAvailable = true},
                 new SmenaDTO(){number = 4, slogan="ПОБЕЖДАЙ", title = "Легенды Зарницы", description = "Напомнит об ответственности за себя и своих близких.", dates = "6-9 июля", color = "#FD7D32", availableTill = "26 июня", isAvailable = true},
                 new SmenaDTO(){number = 5, slogan="УПРАВЛЯЙ", title = "Студенческое самоуправление", description = "Поможет создать команду и развить лидерские качества.", dates = "11-14 июля", color = "#FD7D32", availableTill = "1 июля", isAvailable = true},
